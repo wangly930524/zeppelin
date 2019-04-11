ea6010ac139a2724d5eb1bb565e04e29d2427ff4
lqSunny <Sunnylee0915@163.com>
2019-04-11 14:43:12 +0800

    url控制head显示

--- zeppelin-web/src/app/global/global.js
+++ zeppelin-web/src/app/global/global.js
@@ -1,7 +1,21 @@
 const globalVal = {
-  'headIsShow': true,		// 头部是否显示
+  'headIsShow': false,		// 头部是否显示
   cube(x) {
     return x*x*x;
   },
 };
+
+// 获取url
+function getQueryVariable(variable){
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return(false);
+}
+
+globalVal.headIsShow = getQueryVariable("isShow") == "true";
+
 export {globalVal};
